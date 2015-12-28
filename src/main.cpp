a9c8b19c1df626df64a1659df213da0ba7f4e8b7
SpoonDoctor <davidsonc18@gmail.com>
2015-12-28 18:02:30 -0500

    Repushed changes from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Python.h>
 #include "hrdint/oleddisp/oleddisp.h"
+#include "hrdint/slowscan/slowscan.h"
 
 int main() {
 
@@ -10,6 +11,13 @@ int main() {
 	OledDisp* test = new OledDisp();
 	test->PrintText(PyUnicode_FromString("Hello World!"));
 	delete test;
+
+
+/*	//Create a new instance of OLED Display and Print "Hello World!" to it.
+	SlowScan* test2 = new SlowScan();
+	test2->playFile(PyUnicode_FromString(<file_path name>));
+	delete test2;
+*/
 
     // Finish the Python Interpreter
     Py_Finalize();
