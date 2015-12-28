358f9dd45be63f8c7eeed9d33bb4e210809a83d5
gyurgyma <gyurgyma@mail.uc.edu>
2015-12-28 17:47:32 -0500

    Added one Freaking Comment
    Added a description prior to being forced to commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     // Initialize the Python Interpreter
     Py_Initialize();
 	
+	//Create a new instance of OLED Display and Print "Hello World!" to it.
 	OledDisp* test = new OledDisp();
 	test->PrintText(PyUnicode_FromString("Hello World!"));
 	delete test;
