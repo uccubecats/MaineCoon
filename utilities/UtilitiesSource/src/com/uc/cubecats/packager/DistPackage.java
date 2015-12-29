56fed7a9af7b78835ec7b8beb724d5ca5e039f43
parceltb <Parcell.Tyler@gmail.com>
2015-12-29 10:35:19 -0500

    Added help text to the packaging utility and imroved its logging.

--- utilities/UtilitiesSource/src/com/uc/cubecats/packager/DistPackage.java
+++ utilities/UtilitiesSource/src/com/uc/cubecats/packager/DistPackage.java
@@ -32,6 +32,21 @@ public class DistPackage {
 	 */
 	public static void main(String[] args) {
 		
+		/* Determine if "--help" was passed into the program as an argument. */
+		boolean wasHelpSpecified = false;
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].compareToIgnoreCase("--help") == 0) {
+				wasHelpSpecified = true;
+				break;
+			}
+		}
+		
+		/* If "--help" was specified as an argument, then print the help text and exit the program. */
+		if(wasHelpSpecified) {
+			printHelpText();
+			return;
+		}
+		
 		/* Get a reference to the build path. */
 		File buildDir = new File(PATH_IN);
 		
@@ -56,14 +71,18 @@ public class DistPackage {
 			
 			/* Attempt to copy the file to the "dist" directory. */
 			try {
+				printConsoleOutput("Copying: " + copyList.get(i).getName());
 				copyFile(copyList.get(i), new File(PATH_OUT + File.separator + copyList.get(i).getName()));
+				printConsoleOutput("Copied: " + copyList.get(i).getName());
 				
 				/* If it was a linux/unix executable, "fix it" after copying it. */
 				if(!copyList.get(i).getName().contains(".")) {
 					Process p;
 					try {
+						printConsoleOutput("Running 'chmod': " + copyList.get(i).getName());
 						p = Runtime.getRuntime().exec("chmod +x " + String.valueOf(PATH_OUT + File.separator + copyList.get(i).getName()));
 						p.waitFor();
+						printConsoleOutput("Ran 'chmod': " + copyList.get(i).getName());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -90,10 +109,11 @@ public class DistPackage {
 				/* Try to copy the files to the dist directory. */
 				for(int j = 0; j < pythonFilesList.size(); j++) {
 					try {
-						System.out.println("Copying: " + relativePath.get(j) + File.separator + pythonFilesList.get(j).getName());
+						printConsoleOutput("Copying: " + relativePath.get(j) + File.separator + pythonFilesList.get(j).getName());
 						File rootFile = new File(PATH_OUT + File.separator + relativePath.get(j));
 						rootFile.mkdirs();
 						copyFile(pythonFilesList.get(j), new File(rootFile.getAbsolutePath() + File.separator + pythonFilesList.get(j).getName()));
+						printConsoleOutput("Copied: " + relativePath.get(j) + File.separator + pythonFilesList.get(j).getName());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -123,16 +143,20 @@ public class DistPackage {
 				
 				/* Check to see if the current file is a library, if so, add it. */
 				if(subDirs[i].getAbsolutePath().toLowerCase().endsWith(".so")) {
+					printConsoleOutput("Discovered Library: " + subDirs[i].getName());
 					copyList.add(subDirs[i]);
 				} else if(subDirs[i].getAbsolutePath().toLowerCase().endsWith(".dll")) {
+					printConsoleOutput("Discovered Library: " + subDirs[i].getName());
 					copyList.add(subDirs[i]);
 				
 				/* Check to see if the current file is an executable file, if so, add it. */
 				} else if(subDirs[i].canExecute()) {
 					if(subDirs[i].getAbsolutePath().toLowerCase().endsWith(".exe")) {
+						printConsoleOutput("Discovered Executable: " + subDirs[i].getName());
 						copyList.add(subDirs[i]);
 					} else if(!subDirs[i].getName().toLowerCase().contains(".")) {
 						if(subDirs[i].getName().compareToIgnoreCase("Makefile") != 0) {
+							printConsoleOutput("Discovered Executable: " + subDirs[i].getName());
 							copyList.add(subDirs[i]);
 						}
 					}
@@ -210,6 +234,14 @@ public class DistPackage {
 		
 	}
 	
+	/**
+	 * Function used to create a list of the Python scripts to the dist directory.
+	 *
+	 * @param currDirFull File used to specify the current directory to be referenced by the function.
+	 * @param fileList List of Files used to keep track of the Files (scripts) to copy.
+	 * @param relativePath List of Strings used to keep track of a local directory structure for copying.
+	 * @param currDir String used to keep track of the local, relative path for building the relativePath list.
+	 */
 	private static void findPyFiles(File currDirFull, List<File> fileList, List<String> relativePath, String currDir) {
 		
 		/* Get a list of all files and directories in the current file. */
@@ -221,6 +253,7 @@ public class DistPackage {
 				
 				/* Determine if the current file in the directory is a python script and add it if so. */
 				if(subDirs[i].getName().toLowerCase().endsWith(".py")) {
+					printConsoleOutput("Discovered Python Script: " + currDir + File.separator + subDirs[i].getName());
 					fileList.add(subDirs[i]);
 					relativePath.add(currDir);
 				}
@@ -232,4 +265,23 @@ public class DistPackage {
 		}
 	}
 	
+	/**
+	 * Function used to print updates to the console with a tag for the application.
+	 */
+	private static void printConsoleOutput(String consoleText) {
+		System.out.println("    [DistPackage]: " + consoleText);
+	}
+	
+	/**
+	 * Function used to print the help text to the terminal or command-prompt.
+	 */
+	private static void printHelpText() {
+		System.out.println("");
+		System.out.println("This utility is designed to copy the required files from the '../build' directory to the '../dist' directory.");
+		System.out.println("Usage: java -jar DistPackage.jar [ARG 1] [ARG 2]...");
+		System.out.println("");
+		System.out.println("Each 'ARG' listed in the usage section refers to a sub-directory in the project's src folder.");
+		System.out.println("These directories are the search locations for any Python scripts which are required to be copied (recursively).");
+		System.out.println("");
+	}
 }
