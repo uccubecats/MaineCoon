448553810bbeceb2ef89335b0788bd15447ce55f
parceltb <Parcell.Tyler@gmail.com>
2015-12-29 11:18:30 -0500

    Added the string creation functionality to the AprsPacketUtil library for the coeffs and x-value.

--- src/hrdint/uharp/AprsPacketUtil.cpp
+++ src/hrdint/uharp/AprsPacketUtil.cpp
@@ -1,7 +1,7 @@
 #include "AprsPacketUtil.h"
 
 // Implementation of the absolute value function.
-float abs(float x) {
+float aprs::abs(float x) {
 	if(x < 0) {
 		return -1*x;
 	}
@@ -9,7 +9,7 @@ float abs(float x) {
 }
 
 // Function used to get the best c-coefficent given the best a, b, and x values.
-int getBestC(int a, int b, int x, float v) {
+int aprs::getBestC(int a, int b, int x, float v) {
 	int bestC = -128;
 	for(int c = -127; c < 128; c++) {
 		if(abs((a*x*x + b*x + c) - v) < abs((a*x*x + b*x + bestC) - v)) {
@@ -20,7 +20,7 @@ int getBestC(int a, int b, int x, float v) {
 }
 
 // Function used to get the best b-ceofficent given the best a and x values.
-int getBestB(int a, int x, float v) {
+int aprs::getBestB(int a, int x, float v) {
 	int bestB = -128;
 	for(int b = -127; b < 128; b++) {
 		if(abs((a*x*x + b*x) - v) < abs((a*x*x + bestB*x) - v)) {
@@ -31,7 +31,7 @@ int getBestB(int a, int x, float v) {
 }
 
 // Function used to get the best a-ceofficent and x-value assuming b and c are zero.
-int getBestA(float v, int& xVal) {
+int aprs::getBestA(float v, int& xVal) {
 	int a, x;
 	int bestA = -128;
 	for(a = -127; a < 128; a++) {
@@ -46,7 +46,7 @@ int getBestA(float v, int& xVal) {
 }
 
 // Function used to get the coefficents and x-value in APRS format for a given value.
-int* getAprsValues(float value) {
+int* aprs::getAprsValues(float value) {
 	
 	// Create the array to return.
 	int* aprsValues = new int[4];
@@ -61,4 +61,66 @@ int* getAprsValues(float value) {
 	
 	// Return the APRS values.
 	return aprsValues;
+}
+
+// Function used to get the coefficent string in the APRS packet format.
+unsigned int aprs::getCoeffString(char* buffer, std::string callTag, int* aArray, int* bArray, int* cArray) {
+	
+	// Create a string to perform all initial manipulations on.
+	std::string tmpString = ":" + callTag + " :EQNS.";
+	for(int i = 0; i < 5; i++) {
+		std::ostringstream newStream;
+		newStream << aArray[i] << ",";
+		newStream << bArray[i] << ",";
+		newStream << cArray[i] << ((i < 4) ? "," : "");
+		tmpString += newStream.str();
+	}
+	
+	// Extract the characters from the string and its length.
+	const char* tmpPointer = tmpString.c_str();
+	unsigned int sizeOfBuffer = tmpString.length();
+	
+	// Clear the buffer if it already has data.
+	if(buffer != NULL) {
+		delete[] buffer;
+	}
+	
+	// Copy the string data (using the pointer) to the specified buffer.
+	buffer = new char[sizeOfBuffer];
+	for(int i = 0; i < sizeOfBuffer; i++) {
+		buffer[i] = tmpPointer[i];
+	}
+	
+	// Return the size of the buffer (since we wrote the changes to it).
+	return sizeOfBuffer;
+}
+
+// Function used to get the x string in the APRS packet format.
+unsigned int aprs::getXString(char* buffer, int* xArray) {
+	
+	// Create a string to perform all initial manipulations on.
+	std::string tmpString = "T#";
+	for(int i = 0; i < 5; i++) {
+		std::ostringstream newStream;
+		newStream << xArray[i] << ((i < 4) ? "," : "");
+		tmpString += newStream.str();
+	}
+	
+	// Extract the characters from the string and its length.
+	const char* tmpPointer = tmpString.c_str();
+	unsigned int sizeOfBuffer = tmpString.length();
+	
+	// Clear the buffer if it already has data.
+	if(buffer != NULL) {
+		delete[] buffer;
+	}
+	
+	// Copy the string data (using the pointer) to the specified buffer.
+	buffer = new char[sizeOfBuffer];
+	for(int i = 0; i < sizeOfBuffer; i++) {
+		buffer[i] = tmpPointer[i];
+	}
+	
+	// Return the size of the buffer (since we wrote the changes to it).
+	return sizeOfBuffer;
 }
--- src/hrdint/uharp/AprsPacketUtil.h
+++ src/hrdint/uharp/AprsPacketUtil.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 // Constants
 const int A = 0;	// Index representing the a-ceofficent in an array.
@@ -6,17 +8,27 @@ const int B = 1;	// Index representing the b-ceofficent in an array.
 const int C = 2;	// Index representing the c-ceofficent in an array.
 const int X = 3;	// Index representing the x-value in an array.
 
-// Implementation of the absolute value function.
-float abs(float x);
+// Specify the aprs namespace
+namespace aprs {
 
-// Function used to get the best c-coefficent given the best a, b, and x values.
-int getBestC(int a, int b, int x, float v);
+	// Implementation of the absolute value function.
+	float abs(float x);
 
-// Function used to get the best b-ceofficent given the best a and x values.
-int getBestB(int a, int x, float v);
+	// Function used to get the best c-coefficent given the best a, b, and x values.
+	int getBestC(int a, int b, int x, float v);
 
-// Function used to get the best a-ceofficent and x-value assuming b and c are zero.
-int getBestA(float v, int& xVal);
+	// Function used to get the best b-ceofficent given the best a and x values.
+	int getBestB(int a, int x, float v);
 
-// Function used to get the coefficents and x-value in APRS format for a given value.
-int* getAprsValues(float value);
+	// Function used to get the best a-ceofficent and x-value assuming b and c are zero.
+	int getBestA(float v, int& xVal);
+
+	// Function used to get the coefficents and x-value in APRS format for a given value.
+	int* getAprsValues(float value);
+	
+	// Function used to get the coefficent string in the APRS packet format.
+	unsigned int getCoeffString(char* buffer, std::string callTag, int* aArray, int* bArray, int* cArray);
+	
+	// Function used to get the x string in the APRS packet format.
+	unsigned int getXString(char* buffer, int* xArray);
+};
