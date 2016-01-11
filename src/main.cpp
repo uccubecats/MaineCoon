514740da077680883fe1f41dc81f02e7ec3eb322
gyurgyma <gyurgyma@mail.uc.edu>
2016-01-11 17:45:38 -0500

    Test for Mux stuff
    Added a test for the Mux selection grace made.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <Python.h>
 #include "hrdint/oleddisp/oleddisp.h"
 #include "hrdint/slowscan/slowscan.h"
+#include "hrdint/tempPress/tempPress.h"
+#include <iostream>
 
 int main() {
 
     // Initialize the Python Interpreter
     Py_Initialize();
 	
+	int Temp;
+	
+	//Test for Graces code.
+	Mux* test = new Mux();
+	Temp = test -> getTemperature(0);
+	cout << Temp;
+	Temp = test -> getTemperature(1);
+	cout << Temp;
+	
 	//Create a new instance of OLED Display and Print "Hello World!" to it.
-	OledDisp* test = new OledDisp();
-	test->PrintText(PyUnicode_FromString("Hello World!"));
-	delete test;
+	//OledDisp* test = new OledDisp();
+	//test->PrintText(PyUnicode_FromString("Hello World!"));
+	//delete test;
 
 
 /*	//Create a new instance of OLED Display and Print "Hello World!" to it.
