0f316321b7fd2dcf14fb1b138c7c766fbbb8e353
gyurgyma <gyurgyma@mail.uc.edu>
2016-01-11 16:11:09 -0500

    Readded the code for the OLED Display
    Changed "pyoleddisp.py" so that the print text function prints the text
    to the OLED Display rather than just python printing.

--- src/hrdint/oleddisp/pyoleddisp.py
+++ src/hrdint/oleddisp/pyoleddisp.py
@@ -11,6 +11,14 @@ class OledDisp:
 		self.RESET_PIN = 15
 		self.DC_PIN    = 16
 		
+		# Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
+		self.led = SSD1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
+		self.led.begin()
+		self.led.clear_display() # This clears the display but only when there is a led.display() as well!
+		
 	def PrintText(self, text):
-		print(self.RESET_PIN)
-		print(text)
+		# led.draw_text2(x-axis, y-axis, whatyouwanttoprint, size) < Understand?
+		# So led.drawtext2() prints simple text to the OLED display like so:
+		#text = 'Hello!'
+		self.led.draw_text2(0,0,text,2)
+		self.led.display()
